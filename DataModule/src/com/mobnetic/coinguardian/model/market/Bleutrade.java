461973ba1729cd788142743d411bbe28c8042250
Maciej Ciemięga <ruwenzori@gmail.com>
2014-05-11 11:08:06 +0200

    Changes in Bleutrade

--- DataModule/src/com/mobnetic/coinguardian/model/market/Bleutrade.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/Bleutrade.java
@@ -15,8 +15,7 @@ public class Bleutrade extends Market {
 	
 	private final static String NAME = "Bleutrade";
 	private final static String TTS_NAME = NAME;
-	private final static String URL = "https://bleutrade.com/api/v1/last_price?market=%1$s_%2$s";
-	private final static String URL_2 = "https://bleutrade.com/api/v1/24h_trade_pair_info?market=%1$s_%2$s";
+	private final static String URL = "https://bleutrade.com/api/v1/24h_trade_pair_info?market=%1$s_%2$s";
 	private final static String URL_CURRENCY_PAIRS = "https://bleutrade.com/api/v1/trade_pairs";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	
@@ -25,27 +24,17 @@ public class Bleutrade extends Market {
 	}
 	
 	@Override
-	public int getNumOfRequests(CheckerInfo checkerInfo) {
-		return 2;
-	}
-	
-	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		if(requestId==1)
-			return String.format(URL_2, checkerInfo.getCurrencyBase(), checkerInfo.getCurrencyCounter());
 		return String.format(URL, checkerInfo.getCurrencyBase(), checkerInfo.getCurrencyCounter());
 	}
 	
 	@Override
 	protected void parseTicker(int requestId, String responseString, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		if(requestId==1) {
-			final String[] split = responseString.split(";");
-			ticker.vol = parseDouble(split[5]);
-			ticker.high = parseDouble(split[2]);
-			ticker.low = parseDouble(split[3]);
-		} else {
-			ticker.last = parseDouble(responseString);
-		}
+		final String[] split = responseString.split(";");
+		ticker.vol = parseDouble(split[5]);
+		ticker.high = parseDouble(split[2]);
+		ticker.low = parseDouble(split[3]);
+		ticker.last = parseDouble(split[4]);
 	}
 	
 	private double parseDouble(String doubleString) {
