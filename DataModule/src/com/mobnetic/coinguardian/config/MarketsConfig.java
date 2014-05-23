92585d7ba059d3335b4c4631a8c5dd66a8af8c7d
Maciej Ciemięga <ruwenzori@gmail.com>
2014-05-23 21:53:50 +0200

    Temporarily disabled Bleutrade due to API issues.

--- DataModule/src/com/mobnetic/coinguardian/config/MarketsConfig.java
+++ DataModule/src/com/mobnetic/coinguardian/config/MarketsConfig.java
@@ -16,7 +16,6 @@ import com.mobnetic.coinguardian.model.market.Bitcurex;
 import com.mobnetic.coinguardian.model.market.Bitfinex;
 import com.mobnetic.coinguardian.model.market.Bitorado;
 import com.mobnetic.coinguardian.model.market.Bitstamp;
-import com.mobnetic.coinguardian.model.market.Bleutrade;
 import com.mobnetic.coinguardian.model.market.BtcMarkets;
 import com.mobnetic.coinguardian.model.market.Btcchina;
 import com.mobnetic.coinguardian.model.market.Btce;
@@ -113,7 +112,7 @@ public class MarketsConfig {
 		addMarket(new BitBay());
 		addMarket(new CryptoAltex());
 		addMarket(new BtcMarkets());
-		addMarket(new Bleutrade());
+//		addMarket(new Bleutrade());
 		addMarket(new SwissCex());
 	}
 }
