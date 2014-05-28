9f36bdeaea6bf56e31266e77995d30e530105260
Maciej Ciemięga <ruwenzori@gmail.com>
2014-05-28 22:58:31 +0200

    Unisend - Fixes #73

---
+++ DataModule/src/com/mobnetic/coinguardian/model/market/Unisend.java
@@ -0,0 +1,42 @@
+package com.mobnetic.coinguardian.model.market;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import org.json.JSONObject;
+
+import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.Market;
+import com.mobnetic.coinguardian.model.Ticker;
+import com.mobnetic.coinguardian.model.currency.Currency;
+import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
+
+public class Unisend extends Market {
+
+	private final static String NAME = "Unisend";
+	private final static String TTS_NAME = "Uni send";
+	private final static String URL = "https://www.unisend.com/api/price/ar/ars_btc";
+	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
+	static {
+		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
+				Currency.ARS
+			});
+	}
+	
+	public Unisend() {
+		super(NAME, TTS_NAME, CURRENCY_PAIRS);
+	}
+
+	@Override
+	public String getUrl(int requestId, CheckerInfo checkerInfo) {
+		return String.format(URL, checkerInfo.getCurrencyBase(), checkerInfo.getCurrencyCounter());
+	}
+	
+	@Override
+	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
+		final JSONObject pricesJsonObject = jsonObject.getJSONObject("prices");
+		ticker.bid = pricesJsonObject.getDouble("sell");	// reversed!
+		ticker.ask = pricesJsonObject.getDouble("buy");		// reversed!
+		ticker.last = ticker.ask;
+	}
+}
