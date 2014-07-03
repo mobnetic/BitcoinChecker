22d7fcdbc7970b2997150178de438f8c16968930
Maciej Ciemięga <ruwenzori@gmail.com>
2014-07-03 18:02:55 +0200

    Fixed API in Huobi

--- DataModule/src/com/mobnetic/coinguardian/model/market/Huobi.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/Huobi.java
@@ -3,7 +3,7 @@ package com.mobnetic.coinguardian.model.market;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.mobnetic.coinguardian.model.CheckerInfo;
@@ -16,8 +16,8 @@ public class Huobi extends Market {
 
 	private final static String NAME = "Huobi";
 	private final static String TTS_NAME = NAME;
-	private final static String URL_BTC = "https://market.huobi.com/staticmarket/detail.html";
-	private final static String URL_LTC = "https://market.huobi.com/staticmarket/detail_ltc.html";
+	private final static String URL_BTC = "http://market.huobi.com/staticmarket/ticker_btc_json.js";
+	private final static String URL_LTC = "http://market.huobi.com/staticmarket/ticker_ltc_json.js";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
@@ -41,26 +41,18 @@ public class Huobi extends Market {
 	}
 	
 	@Override
-	protected void parseTicker(int requestId, String responseString, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		final String startString = "view_detail(";
-		final String endString = ")";
+	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
+		final JSONObject tickerJsonObject = jsonObject.getJSONObject("ticker");
 		
-		if(responseString!=null && responseString.length()>(startString.length()+endString.length()))
-			responseString = responseString.substring(startString.length(), responseString.length()-endString.length());
-		super.parseTicker(requestId, responseString, ticker, checkerInfo);
+		ticker.bid = getDoubleFromString(tickerJsonObject, "buy");
+		ticker.ask = getDoubleFromString(tickerJsonObject, "sell");
+		ticker.vol = getDoubleFromString(tickerJsonObject, "vol");
+		ticker.high = getDoubleFromString(tickerJsonObject, "high");
+		ticker.low = getDoubleFromString(tickerJsonObject, "low");
+		ticker.last = getDoubleFromString(tickerJsonObject, "last");
 	}
 	
-	@Override
-	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		JSONArray buys = jsonObject.optJSONArray("buys");
-		if(buys!=null && buys.length()>0)
-			ticker.bid = buys.getJSONObject(0).getDouble("price");
-		JSONArray sells = jsonObject.optJSONArray("sells");
-		if(sells!=null && sells.length()>0)
-			ticker.ask = sells.getJSONObject(0).getDouble("price");
-		ticker.vol = jsonObject.getDouble("amount");
-		ticker.high = jsonObject.getDouble("p_high");
-		ticker.low = jsonObject.getDouble("p_low");
-		ticker.last = jsonObject.getDouble("p_last");
+	private double getDoubleFromString(JSONObject jsonObject, String name) throws NumberFormatException, JSONException {
+		return Double.parseDouble(jsonObject.getString(name));
 	}
 }
