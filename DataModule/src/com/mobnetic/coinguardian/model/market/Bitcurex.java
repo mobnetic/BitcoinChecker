ce335abb183f72bc206feb70dd1583416d58e2f3
Maciej Ciemięga <ruwenzori@gmail.com>
2014-09-02 19:32:21 +0200

    Fixed Bitcurex API

--- DataModule/src/com/mobnetic/coinguardian/model/market/Bitcurex.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/Bitcurex.java
@@ -15,11 +15,12 @@ public class Bitcurex extends Market {
 
 	private final static String NAME = "Bitcurex";
 	private final static String TTS_NAME = NAME;
-	private final static String URL = "https://pln.bitcurex.com/data/ticker.json";
+	private final static String URL = "https://bitcurex.com/api/%1$s/ticker.json";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
-				Currency.PLN
+				Currency.PLN,
+				Currency.EUR
 			});
 	}
 	
@@ -29,17 +30,24 @@ public class Bitcurex extends Market {
 
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		return URL;
+		return String.format(URL, checkerInfo.getCurrencyCounterLowerCase());
 	}
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		ticker.bid = jsonObject.getDouble("buy");
-		ticker.ask = jsonObject.getDouble("sell");
-		ticker.vol = jsonObject.getDouble("vol");
-		ticker.high = jsonObject.getDouble("high");
-		ticker.low = jsonObject.getDouble("low");
-		ticker.last = jsonObject.getDouble("last");
-		ticker.timestamp = jsonObject.getLong("time");
+		ticker.bid = parsePrice(jsonObject.getDouble("best_ask"));
+		ticker.ask = parsePrice(jsonObject.getDouble("best_bid"));
+		ticker.vol = parseBTC(jsonObject.getDouble("total_volume"));
+		ticker.high = parsePrice(jsonObject.getDouble("highest_tx_price"));
+		ticker.low = parsePrice(jsonObject.getDouble("lowest_tx_price"));
+		ticker.last = parsePrice(jsonObject.getDouble("last_tx_price"));
+	}
+	
+	private double parsePrice(double price) {
+		return price/10000;
+	}
+	
+	private double parseBTC(double satoshi) {
+		return satoshi/100000000;
 	}
 }
