bed2d8926e5c7b3574e66455f75d93a7099faa5a
Maciej Ciemięga <ruwenzori@gmail.com>
2014-09-14 19:54:26 +0200

    Added dynamic currency pairs fetching to Kraken exchange

--- DataModule/src/com/mobnetic/coinguardian/model/market/Kraken.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/Kraken.java
@@ -1,85 +1,42 @@
 package com.mobnetic.coinguardian.model.market;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
-import com.mobnetic.coinguardian.model.currency.Currency;
 import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
 
 public class Kraken extends Market {
 
 	private final static String NAME = "Kraken";
 	private final static String TTS_NAME = NAME;
-	private final static String URL = "https://api.kraken.com/0/public/Ticker?pair=%1$s%2$s";
-	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
-	static {
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
-				Currency.USD,
-				Currency.EUR,
-				Currency.KRW,
-				VirtualCurrency.LTC,
-				VirtualCurrency.DOGE,
-				VirtualCurrency.NMC,
-				VirtualCurrency.XRP,
-				VirtualCurrency.VEN
-		});
-		CURRENCY_PAIRS.put(Currency.EUR, new String[]{
-				VirtualCurrency.DOGE,
-				VirtualCurrency.XRP,
-				VirtualCurrency.VEN
-			});
-		CURRENCY_PAIRS.put(Currency.KRW, new String[]{
-				VirtualCurrency.XRP
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.LTC, new String[]{
-				Currency.USD,
-				Currency.EUR,
-				Currency.KRW,
-				VirtualCurrency.DOGE,
-				VirtualCurrency.XRP
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.NMC, new String[]{
-				Currency.USD,
-				Currency.EUR,
-				Currency.KRW,
-				VirtualCurrency.DOGE,
-				VirtualCurrency.XRP
-			});
-		CURRENCY_PAIRS.put(Currency.USD, new String[]{
-				VirtualCurrency.DOGE,
-				VirtualCurrency.XRP,
-				VirtualCurrency.VEN
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.VEN, new String[]{
-				VirtualCurrency.XRP
-			});
-	}
+	private final static String URL = "https://api.kraken.com/0/public/Ticker?pair=%1$s";
+	private final static String URL_CURRENCY_PAIRS = "https://api.kraken.com/0/public/AssetPairs";
 	
 	public Kraken() {
-		super(NAME, TTS_NAME, CURRENCY_PAIRS);
+		super(NAME, TTS_NAME, null);
 	}
 	
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		String currencyBase = fixCurrency(checkerInfo.getCurrencyBase());
-		String currencyCounter = fixCurrency(checkerInfo.getCurrencyCounter());
-		
-		return String.format(URL, currencyBase, currencyCounter);
+		if(checkerInfo.getCurrencyPairId()!=null) {
+			return String.format(URL, checkerInfo.getCurrencyPairId()); 
+		} else {
+			final String currencyBase = fixCurrency(checkerInfo.getCurrencyBase());
+			final String currencyCounter = fixCurrency(checkerInfo.getCurrencyCounter());
+			return String.format(URL, currencyBase+currencyCounter);
+		}
 	}
 	
 	private String fixCurrency(String currency) {
-		if(VirtualCurrency.BTC.equals(currency))
-			return VirtualCurrency.XBT;
-		if(VirtualCurrency.VEN.equals(currency))
-			return VirtualCurrency.XVN;
-		if(VirtualCurrency.DOGE.equals(currency))
-			return VirtualCurrency.XDG;
+		if (VirtualCurrency.BTC.equals(currency)) return VirtualCurrency.XBT;
+		if (VirtualCurrency.VEN.equals(currency)) return VirtualCurrency.XVN;
+		if (VirtualCurrency.DOGE.equals(currency)) return VirtualCurrency.XDG;
 		return currency;
 	}
 	
@@ -94,11 +51,44 @@ public class Kraken extends Market {
 		ticker.high = getDoubleFromJsonArrayObject(pairObject, "h");
 		ticker.low = getDoubleFromJsonArrayObject(pairObject, "l");
 		ticker.last = getDoubleFromJsonArrayObject(pairObject, "c");
-//		ticker.timestamp = ; //
 	}
 	
 	private double getDoubleFromJsonArrayObject(JSONObject jsonObject, String arrayKey) throws Exception {
 		final JSONArray jsonArray = jsonObject.getJSONArray(arrayKey);
 		return jsonArray!=null && jsonArray.length()>0 ? jsonArray.getDouble(0) : 0;
+	}
+	
+	// ====================
+    // Get currency pairs
+    // ====================
+    @Override
+    public String getCurrencyPairsUrl(int requestId) {
+        return URL_CURRENCY_PAIRS;
+    }
+    
+    @Override
+    protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+        final JSONObject result = jsonObject.getJSONObject("result");
+        final JSONArray pairNames = result.names();
+        
+        for(int i=0; i<pairNames.length(); ++i) {
+        	final String pairId = pairNames.getString(i);
+        	final JSONObject pairJsonObject = result.getJSONObject(pairId);
+        	
+            pairs.add(new CurrencyPairInfo(
+            		parseCurrency(pairJsonObject.getString("base")),
+            		parseCurrency(pairJsonObject.getString("quote")),
+            		pairId));
+        }
+    }
+    
+    private String parseCurrency(String currency) {
+		if(currency!=null && currency.length()>=2) {
+			currency = currency.substring(1);
+		}
+		if (VirtualCurrency.XBT.equals(currency)) return VirtualCurrency.BTC;
+		if (VirtualCurrency.XVN.equals(currency)) return VirtualCurrency.VEN;
+		if (VirtualCurrency.XDG.equals(currency)) return VirtualCurrency.DOGE;
+		return currency;
 	}
 }
