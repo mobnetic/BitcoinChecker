6e83ff5d46894cdd8ae82a34b67f1ecb645ab3e4
Maciej Ciemięga <ruwenzori@gmail.com>
2014-10-14 21:35:52 +0200

    MintPal v2 API

--- DataModule/src/com/mobnetic/coinguardian/model/market/MintPal.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/MintPal.java
@@ -14,8 +14,8 @@ public class MintPal extends Market {
 
 	private final static String NAME = "MintPal";
 	private final static String TTS_NAME = "Mint Pal";
-	private final static String URL = "https://api.mintpal.com/market/stats/%1$s/%2$s/";
-	private final static String URL_CURRENCY_PAIRS = "https://api.mintpal.com/market/summary/";
+	private final static String URL = "https://v2.mintpal.com/api/v2/public/market/%1$s/%2$s/summary";
+	private final static String URL_CURRENCY_PAIRS = "https://v2.mintpal.com/api/v2/public/markets/overview";
 	
 	public MintPal() {
 		super(NAME, TTS_NAME, null);
@@ -33,10 +33,12 @@ public class MintPal extends Market {
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		ticker.vol = jsonObject.getDouble("24hvol");
-		ticker.high = jsonObject.getDouble("24hhigh");
-		ticker.low = jsonObject.getDouble("24hlow");
-		ticker.last = jsonObject.getDouble("last_price");
+		ticker.bid = jsonObject.getDouble("bid");
+		ticker.ask = jsonObject.getDouble("ask");
+		ticker.vol = jsonObject.getDouble("volume");
+		ticker.high = jsonObject.getDouble("high");
+		ticker.low = jsonObject.getDouble("low");
+		ticker.last = jsonObject.getDouble("last");
 	}
 	
 	// ====================
@@ -53,8 +55,8 @@ public class MintPal extends Market {
 		for(int i=0; i<jsonArray.length(); ++i) {
 			JSONObject marketObject = jsonArray.getJSONObject(i);
 			pairs.add(new CurrencyPairInfo(
-					marketObject.getString("code"),
-					marketObject.getString("exchange"),
+					marketObject.getString("pair"),
+					marketObject.getString("coin"),
 					null
 				));
 		}
