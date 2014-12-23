20f133b5ec26d5ec51f8155b519af30baf6ca362
Maciej Ciemięga <ruwenzori@gmail.com>
2014-12-23 23:45:45 +0100

    Added dynamic currency fetching for Bitcoin.co.id

--- DataModule/src/com/mobnetic/coinguardian/model/market/BitcoinCoId.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/BitcoinCoId.java
@@ -1,43 +1,34 @@
 package com.mobnetic.coinguardian.model.market;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
-import com.mobnetic.coinguardian.model.currency.Currency;
-import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
 
 public class BitcoinCoId extends Market {
 
 	private final static String NAME = "Bitcoin.co.id";
 	private final static String TTS_NAME = "Bitcoin co id"; 
-	private final static String URL = "https://vip.bitcoin.co.id/api/%1$s_%2$s/ticker/";
-	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
-	static {
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
-				Currency.IDR
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.DOGE, new String[]{
-				VirtualCurrency.BTC
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.LTC, new String[]{
-				VirtualCurrency.BTC
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.XPY, new String[]{
-				VirtualCurrency.BTC
-			});
-	}
+	private final static String URL = "https://vip.bitcoin.co.id/api/%1$s/ticker/";
+	private final static String URL_CURRENCY_PAIRS = "https://vip.bitcoin.co.id/api/summaries";
 	
 	public BitcoinCoId() {
-		super(NAME, TTS_NAME, CURRENCY_PAIRS);
+		super(NAME, TTS_NAME, null);
 	}
 	
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		return String.format(URL, checkerInfo.getCurrencyBase(), checkerInfo.getCurrencyCounter());
+		String pairId = checkerInfo.getCurrencyPairId();
+		if (pairId == null) {
+			pairId = String.format("%1$s_%2$s", checkerInfo.getCurrencyBaseLowerCase(), checkerInfo.getCurrencyCounterLowerCase());
+		}
+		return String.format(URL, pairId);
 	}
 	
 	@Override
@@ -50,5 +41,31 @@ public class BitcoinCoId extends Market {
 		ticker.low = tickerJsonObject.getDouble("low");
 		ticker.last = tickerJsonObject.getDouble("last");
 		ticker.timestamp = tickerJsonObject.getLong("server_time");
+	}
+	
+	// ====================
+	// Get currency pairs
+	// ====================
+	@Override
+	public String getCurrencyPairsUrl(int requestId) {
+		return URL_CURRENCY_PAIRS;
+	}
+	
+	@Override
+	protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+		final JSONObject tickersJsonObject = jsonObject.getJSONObject("tickers");
+		final JSONArray tickerNamesArray = tickersJsonObject.names();
+		for(int i=0; i<tickerNamesArray.length(); ++i) {
+			final String pairId = tickerNamesArray.getString(i);
+			if(pairId==null)
+				continue;
+			String[] currencies = pairId.split("_");
+			if(currencies.length!=2)
+				continue;
+			
+			String currencyBase = currencies[0].toUpperCase(Locale.ENGLISH);
+			String currencyCounter = currencies[1].toUpperCase(Locale.ENGLISH);
+			pairs.add(new CurrencyPairInfo(currencyBase, currencyCounter, pairId));
+		}
 	}
 }
