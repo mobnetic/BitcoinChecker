03d3523d7029c6febf36a9a1ec11850c71e41688
joseccnet <joseccnet@hotmail.com>
2015-09-18 19:11:39 -0500

    updated code
    Updated code because observations

--- DataModule/src/com/mobnetic/coinguardian/model/market/OkcoinFuturesPrice.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/OkcoinFuturesPrice.java
@@ -12,31 +12,31 @@ import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
 
 public class OkcoinFuturesPrice extends Market {
 
-	private final static String NAME = "OKCoin Futures Price";
-	private final static String TTS_NAME = "Ok coin feature price";
+	private final static String NAME = "OKCoin futures price";
+	private final static String TTS_NAME = "Ok coin features price";
 	private final static String URL_USD_1w = "https://www.okcoin.com/api/v1/future_ticker.do?symbol=%1$s_%2$s&contract_type=this_week";
 	private final static String URL_USD_2w = "https://www.okcoin.com/api/v1/future_ticker.do?symbol=%1$s_%2$s&contract_type=next_week";
 	private final static String URL_USD_3m = "https://www.okcoin.com/api/v1/future_ticker.do?symbol=%1$s_%2$s&contract_type=quarter";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC_1WeekFutures, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.BTCFUT1W, new String[]{
 //				Currency.CNY, //At this time (2015-09-17) no API exist for www.okcoin.cn. Check https://www.okcoin.cn/about/rest_api.do
 				Currency.USD
 			});
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC_2WeeksFutures, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.BTCFUT2W, new String[]{
 				Currency.USD
 			});
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC_3MonthsFutures, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.BTCFUT3M, new String[]{
 				Currency.USD
 			});
-		CURRENCY_PAIRS.put(VirtualCurrency.LTC_1WeekFutures, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.LTCFUT1W, new String[]{
 //				Currency.CNY, //At this time (2015-09-17) no API exist for www.okcoin.cn. Check https://www.okcoin.cn/about/rest_api.do
 				Currency.USD
 			});
-		CURRENCY_PAIRS.put(VirtualCurrency.LTC_2WeeksFutures, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.LTCFUT2W, new String[]{
 				Currency.USD
 			});
-		CURRENCY_PAIRS.put(VirtualCurrency.LTC_3MonthsFutures, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.LTCFUT3M, new String[]{
 				Currency.USD
 			});
 	}
@@ -47,12 +47,12 @@ public class OkcoinFuturesPrice extends Market {
 
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		if(checkerInfo.getCurrencyBase().contains("TC This Week Futures")) {
-		   return String.format(URL_USD_1w, checkerInfo.getCurrencyBaseLowerCase().split(" ")[0], checkerInfo.getCurrencyCounterLowerCase());
-		} else if(checkerInfo.getCurrencyBase().contains("TC Next Week Futures")) {
-		   return String.format(URL_USD_2w, checkerInfo.getCurrencyBaseLowerCase().split(" ")[0], checkerInfo.getCurrencyCounterLowerCase());
-		} else if(checkerInfo.getCurrencyBase().contains("TC Next Quarter Futures")) {
-		   return String.format(URL_USD_3m, checkerInfo.getCurrencyBaseLowerCase().split(" ")[0], checkerInfo.getCurrencyCounterLowerCase());
+		if(checkerInfo.getCurrencyBase().contains("TCFUT1W")) {
+		   return String.format(URL_USD_1w, checkerInfo.getCurrencyBaseLowerCase().split("f")[0], checkerInfo.getCurrencyCounterLowerCase());
+		} else if(checkerInfo.getCurrencyBase().contains("TCFUT2W")) {
+		   return String.format(URL_USD_2w, checkerInfo.getCurrencyBaseLowerCase().split("f")[0], checkerInfo.getCurrencyCounterLowerCase());
+		} else if(checkerInfo.getCurrencyBase().contains("TCFUT3M")) {
+		   return String.format(URL_USD_3m, checkerInfo.getCurrencyBaseLowerCase().split("f")[0], checkerInfo.getCurrencyCounterLowerCase());
 		}
 		return "https://www.okcoin.com/about/rest_api.do"; //URL API - should never happen
 	}
