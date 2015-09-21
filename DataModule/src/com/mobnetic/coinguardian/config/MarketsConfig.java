f8447f3c3b9fd1b256838d26bec72d2e936672c1
joseccnet <joseccnet@hotmail.com>
2015-09-21 13:24:56 -0500

    New exchanger: bitreserve.org
    New contribution for bitreserve.org
    
    Checked working with DataModuleTester.

--- DataModule/src/com/mobnetic/coinguardian/config/MarketsConfig.java
+++ DataModule/src/com/mobnetic/coinguardian/config/MarketsConfig.java
@@ -23,6 +23,7 @@ import com.mobnetic.coinguardian.model.market.BitexLa;
 import com.mobnetic.coinguardian.model.market.Bitfinex;
 import com.mobnetic.coinguardian.model.market.BitMarket24PL;
 import com.mobnetic.coinguardian.model.market.Bitorado;
+import com.mobnetic.coinguardian.model.market.Bitreserve;
 import com.mobnetic.coinguardian.model.market.Bitso;
 import com.mobnetic.coinguardian.model.market.Bitstamp;
 import com.mobnetic.coinguardian.model.market.BitxCom;
@@ -108,6 +109,7 @@ public class MarketsConfig {
 		addMarket(new Mercado());
 		addMarket(new Kraken());
 		addMarket(new Bitfinex());
+		addMarket(new Bitreserve());
 		addMarket(new Okcoin());
 		addMarket(new Cryptsy());
 		addMarket(new Coinbase());
