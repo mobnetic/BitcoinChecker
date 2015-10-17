bae87fe2e505c30f0aa78b38e06f48e3529491f0
Maciej Ciemięga <ciemiega.m@gmail.com>
2015-10-17 14:13:45 +0200

    Minor improvements

--- DataModule/src/com/mobnetic/coinguardian/model/Futures.java
+++ DataModule/src/com/mobnetic/coinguardian/model/Futures.java
@@ -8,12 +8,19 @@ public class Futures {
 	public final static int CONTRACT_TYPE_BIMONTHLY = 3;
 	public final static int CONTRACT_TYPE_QUARTERLY = 4;
 	
-	public final static String[] CONTRACT_TYPE_SHORT_NAMES = new String[] {
+	private final static String[] CONTRACT_TYPE_SHORT_NAMES = new String[] {
 		"1W",
 		"2W",
 		"1M",
 		"2M",
 		"3M"
 	};
+	
+	public static String getContractTypeShortName(int contractType) {
+		if (contractType >=0 && contractType < CONTRACT_TYPE_SHORT_NAMES.length) {
+			return CONTRACT_TYPE_SHORT_NAMES[contractType];
+		}
+		return null;
+	}
 	
 }
