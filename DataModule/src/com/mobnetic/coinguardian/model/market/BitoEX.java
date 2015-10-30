e75518968dafd0d7a66f0a06e64be48722dfd1b1
pingu8007 <pingu8007@gmail.com>
2015-10-30 17:29:09 +0800

    Create BitoEX.java

---
+++ DataModule/src/com/mobnetic/coinguardian/model/market/BitoEX.java
@@ -0,0 +1,41 @@
+package com.mobnetic.coinguardian.model.market;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import org.json.JSONObject;
+
+import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.Market;
+import com.mobnetic.coinguardian.model.Ticker;
+import com.mobnetic.coinguardian.model.currency.Currency;
+import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
+
+public class BitoEX extends Market {
+
+	private final static String NAME = "BitoEX";
+	private final static String TTS_NAME = NAME; 
+	private final static String URL = "https://www.bitoex.com/sync/dashboard/0";
+	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
+	static {
+		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
+				Currency.TWD
+			});
+	}
+	
+	public BitoEX() {
+		super(NAME, TTS_NAME, CURRENCY_PAIRS);
+	}
+	
+	@Override
+	public String getUrl(int requestId, CheckerInfo checkerInfo) {
+		return URL;
+	}
+	
+	@Override
+	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
+		ticker.bid = jsonObject.getDouble(1);
+		ticker.ask = jsonObject.getDouble(0);
+		ticker.last = jsonObject.getDouble(0);
+		ticker.timestamp = jsonObject.getLong(2);
+	}
+}
