b302e5c38eff947da6e094d902733771578891f5
pingu8007 <pingu8007@gmail.com>
2015-10-30 20:35:01 +0800

    New Exchange: BitoEX

--- DataModule/src/com/mobnetic/coinguardian/model/market/BitoEX.java
+++ DataModule/src/com/mobnetic/coinguardian/model/market/BitoEX.java
@@ -1,7 +1,10 @@
 package com.mobnetic.coinguardian.model.market;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mobnetic.coinguardian.model.CheckerInfo;
@@ -14,7 +17,7 @@ public class BitoEX extends Market {
 
 	private final static String NAME = "BitoEX";
 	private final static String TTS_NAME = NAME; 
-	private final static String URL = "https://www.bitoex.com/sync/dashboard/0";
+	private final static String URL = "https://www.bitoex.com/sync/dashboard/%1$s";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
@@ -28,14 +31,25 @@ public class BitoEX extends Market {
 	
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		return URL;
+		return String.format(URL, new Date().getTime());
 	}
-	
+
+	@Override
+	protected void parseTicker(int requestId, String responseString, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
+		JSONArray jsonArray = new JSONArray(responseString);
+		JSONObject jsonContainer = new JSONObject();
+		jsonContainer.put("ask",jsonArray.getString(0).replace(",", ""));
+		jsonContainer.put("bid",jsonArray.getString(1).replace(",", ""));
+		jsonContainer.put("last",jsonArray.getString(0).replace(",", ""));
+		jsonContainer.put("timestamp", jsonArray.getString(2));
+		parseTickerFromJsonObject(requestId, jsonContainer, ticker, checkerInfo);
+	}
+
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		ticker.bid = jsonObject.getDouble(1);
-		ticker.ask = jsonObject.getDouble(0);
-		ticker.last = jsonObject.getDouble(0);
-		ticker.timestamp = jsonObject.getLong(2);
+		ticker.ask = jsonObject.getDouble("ask");
+		ticker.bid = jsonObject.getDouble("bid");
+		ticker.last =  jsonObject.getDouble("last");
+		ticker.timestamp = jsonObject.getLong("timestamp");
 	}
 }
