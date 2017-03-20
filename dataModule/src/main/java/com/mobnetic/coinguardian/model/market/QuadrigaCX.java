c9c4820cd4a14d79bf93b9640bcd384049b56b1c
Ivan Salazar <iesalazars@gmail.com>
2017-03-20 11:27:38 -0400

    Update QuadrigaCX.java

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/QuadrigaCX.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/QuadrigaCX.java
@@ -19,6 +19,10 @@ public class QuadrigaCX extends Market {
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
+				Currency.CAD,
+				Currency.USD
+			});
+		CURRENCY_PAIRS.put(VirtualCurrency.ETH, new String[]{
 				Currency.CAD
 			});
 	}
