a1e42cbe74ae933db6fd4e0a69f9503583e649ff
Maciej Ciemięga <ciemiega.m@gmail.com>
2017-03-25 09:07:05 +0100

    Added dynamic currency pairs fetching to CexIO (#293)

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/CexIO.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/CexIO.java
@@ -2,10 +2,13 @@ package com.mobnetic.coinguardian.model.market;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
 import com.mobnetic.coinguardian.model.currency.Currency;
@@ -16,6 +19,7 @@ public class CexIO extends Market {
 	private final static String NAME = "CEX.IO";
 	private final static String TTS_NAME = "CEX IO";
 	private final static String URL = "https://cex.io/api/ticker/%1$s/%2$s";
+	private final static String URL_CURRENCY_PAIRS = "https://cex.io/api/currency_limits";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
@@ -55,5 +59,30 @@ public class CexIO extends Market {
 		ticker.high = jsonObject.getDouble("high");
 		ticker.low = jsonObject.getDouble("low");
 		ticker.last = jsonObject.getDouble("last");
+	}
+
+	// ====================
+	// Get currency pairs
+	// ====================
+	@Override
+	public String getCurrencyPairsUrl(int requestId) {
+		return URL_CURRENCY_PAIRS;
+	}
+
+	@Override
+	protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+		final JSONObject dataJsonObject = jsonObject.getJSONObject("data");
+		final JSONArray pairsJsonArray = dataJsonObject.getJSONArray("pairs");
+		for(int i=0; i<pairsJsonArray.length(); ++i) {
+			final JSONObject pairJsonObject = pairsJsonArray.getJSONObject(i);
+			final String currencyBase = pairJsonObject.getString("symbol1");
+			final String currencyCounter = pairJsonObject.getString("symbol2");
+			if(currencyBase != null && currencyCounter != null) {
+				pairs.add(new CurrencyPairInfo(
+						currencyBase,
+						currencyCounter,
+						null));
+			}
+		}
 	}
 }
