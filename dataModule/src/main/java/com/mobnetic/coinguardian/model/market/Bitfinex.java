93a3d381cce578a7f7e9660aa9e06f2f598449d5
Yield <yield65@users.noreply.github.com>
2017-03-27 12:48:38 -0600

    Adding support for XMR
    Adding support for XMR/BTC and XMR/USD.

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bitfinex.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bitfinex.java
@@ -31,6 +31,10 @@ public class Bitfinex extends Market {
 				Currency.USD,
 				VirtualCurrency.BTC
 			});
+		CURRENCY_PAIRS.put(VirtualCurrency.XMR, new String[]{
+				VirtualCurrency.BTC,
+				Currency.USD
+		});
 		CURRENCY_PAIRS.put(VirtualCurrency.ETC, new String[]{
 				VirtualCurrency.BTC,
 				Currency.USD
