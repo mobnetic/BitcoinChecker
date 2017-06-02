e1133f295168ff3b47e90bcac68ebf1b79211bb6
Maciej Ciemięga <MaciejCiemiega@users.noreply.github.com>
2017-06-02 22:48:01 +0200

    Updated TheRock API to v1. Fixes #327

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/TheRock.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/TheRock.java
@@ -1,22 +1,25 @@
 package com.mobnetic.coinguardian.model.market;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
 import com.mobnetic.coinguardian.model.currency.Currency;
 import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class TheRock extends Market {
 
 	private final static String NAME = "TheRock";
 	private final static String TTS_NAME = "The Rock";
-	private final static String URL = "https://www.therocktrading.com/api/ticker/%1$s%2$s";
+	private final static String URL = "https://api.therocktrading.com/v1/funds/%1$s/ticker";
+	private final static String URL_CURRENCY_PAIRS = "https://api.therocktrading.com/v1/funds";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
@@ -50,25 +53,48 @@ public class TheRock extends Market {
 
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		return String.format(URL, fixCurrency(checkerInfo.getCurrencyBase()), fixCurrency(checkerInfo.getCurrencyCounter()));
+		String pairId = checkerInfo.getCurrencyPairId();
+		if (pairId == null) {
+			pairId = fixCurrency(checkerInfo.getCurrencyBase()) + fixCurrency(checkerInfo.getCurrencyCounter());
+		}
+		return String.format(URL, pairId);
 	}
 	
 	private String fixCurrency(String currency) {
-		if(VirtualCurrency.DOGE.equals(currency))
+		if(VirtualCurrency.DOGE.equals(currency)) {
 			return VirtualCurrency.DOG;
+		}
 		return currency;
 	}
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		final JSONArray resultArray = jsonObject.getJSONArray("result");
-		final JSONObject tickerObject = resultArray.getJSONObject(0);
-		
-		ticker.bid = tickerObject.getDouble("bid");
-		ticker.ask = tickerObject.getDouble("ask");
-		ticker.vol = tickerObject.getDouble("volume");
-		ticker.high = tickerObject.getDouble("high");
-		ticker.low = tickerObject.getDouble("low");
-		ticker.last = tickerObject.getDouble("last");
+		ticker.bid = jsonObject.getDouble("bid");
+		ticker.ask = jsonObject.getDouble("ask");
+		ticker.vol = jsonObject.getDouble("volume");
+		ticker.high = jsonObject.getDouble("high");
+		ticker.low = jsonObject.getDouble("low");
+		ticker.last = jsonObject.getDouble("last");
+	}
+
+	// ====================
+	// Get currency pairs
+	// ====================
+	@Override
+	public String getCurrencyPairsUrl(int requestId) {
+		return URL_CURRENCY_PAIRS;
+	}
+
+	@Override
+	protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+		final JSONArray fundsJsonArray = jsonObject.getJSONArray("funds");
+		for (int i = 0; i < fundsJsonArray.length(); i++) {
+			final JSONObject pairJsonObject = fundsJsonArray.getJSONObject(i);
+			pairs.add(new CurrencyPairInfo(
+					pairJsonObject.getString("trade_currency"),
+					pairJsonObject.getString("base_currency"),
+					pairJsonObject.getString("id"))
+			);
+		}
 	}
 }
