7e22a5750e14e0e084fe46b0a9c011b8b5a463ce
Maciej Ciemięga <MaciejCiemiega@users.noreply.github.com>
2017-11-03 20:45:26 +0100

    Migrated Bter to Gate.io

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bter.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bter.java
@@ -13,10 +13,10 @@ import com.mobnetic.coinguardian.model.Ticker;
 
 public class Bter extends Market {
 
-	private final static String NAME = "Bter";
-	private final static String TTS_NAME = "B ter";
-	private final static String URL = "https://data.bter.com/api/1/ticker/%1$s_%2$s";
-	private final static String URL_CURRENCY_PAIRS = "https://data.bter.com/api/1/pairs";
+	private final static String NAME = "Gate.io";
+	private final static String TTS_NAME = "Gate io";
+	private final static String URL = "http://data.gate.io/api2/1/ticker/%1$s_%2$s";
+	private final static String URL_CURRENCY_PAIRS = "http://data.gate.io/api2/1/pairs";
 
 	public Bter() {
 		super(NAME, TTS_NAME, null);
@@ -29,11 +29,11 @@ public class Bter extends Market {
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		ticker.bid = jsonObject.getDouble("buy");
-		ticker.ask = jsonObject.getDouble("sell");
-		ticker.vol = jsonObject.getDouble("vol_"+checkerInfo.getCurrencyBaseLowerCase());
-		ticker.high = jsonObject.getDouble("high");
-		ticker.low = jsonObject.getDouble("low");
+		ticker.bid = jsonObject.getDouble("highestBid");
+		ticker.ask = jsonObject.getDouble("lowestAsk");
+		ticker.vol = jsonObject.getDouble("quoteVolume");
+		ticker.high = jsonObject.getDouble("high24hr");
+		ticker.low = jsonObject.getDouble("low24hr");
 		ticker.last = jsonObject.getDouble("last");
 	}
 	
