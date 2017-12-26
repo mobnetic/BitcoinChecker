0bebd309eb877e9b8f5889610fa4ae8e63caade7
Maciej Ciemięga <MaciejCiemiega@users.noreply.github.com>
2017-12-26 10:13:59 +0100

    Added currency pairs fetching to Bitso

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bitso.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bitso.java
@@ -2,10 +2,14 @@ package com.mobnetic.coinguardian.model.market;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
 import com.mobnetic.coinguardian.model.currency.Currency;
@@ -17,7 +21,7 @@ public class Bitso extends Market {
 	private final static String NAME = "Bitso";
 	private final static String TTS_NAME = NAME;
 	private final static String URL = "https://api.bitso.com/public/info";
-	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>(1);
+	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
 				Currency.MXN
@@ -50,10 +54,40 @@ public class Bitso extends Market {
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		final JSONObject pairJsonObject = jsonObject.getJSONObject(checkerInfo.getCurrencyBaseLowerCase()+"_"+checkerInfo.getCurrencyCounterLowerCase());
+		String pairId = checkerInfo.getCurrencyPairId();
+		if (pairId == null) {
+			pairId = checkerInfo.getCurrencyBaseLowerCase()+"_"+checkerInfo.getCurrencyCounterLowerCase();
+		}
+		final JSONObject pairJsonObject = jsonObject.getJSONObject(pairId);
 		ticker.bid = ParseUtils.getDoubleFromString(pairJsonObject, "buy");
 		ticker.ask = ParseUtils.getDoubleFromString(pairJsonObject, "sell");
 		ticker.vol = ParseUtils.getDoubleFromString(pairJsonObject, "volume");
 		ticker.last = ParseUtils.getDoubleFromString(pairJsonObject, "rate");
+	}
+
+	// ====================
+	// Get currency pairs
+	// ====================
+	@Override
+	public String getCurrencyPairsUrl(int requestId) {
+		return URL;
+	}
+
+	@Override
+	protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+		final JSONArray pairIds = jsonObject.names();
+		for(int i=0; i<pairIds.length(); ++i) {
+			String pairId = pairIds.getString(i);
+			if(pairId==null)
+				continue;
+			String[] currencies = pairId.split("_");
+			if(currencies.length != 2)
+				continue;
+
+			pairs.add(new CurrencyPairInfo(
+					currencies[0].toUpperCase(Locale.US),
+					currencies[1].toUpperCase(Locale.US),
+					pairId));
+		}
 	}
 }
