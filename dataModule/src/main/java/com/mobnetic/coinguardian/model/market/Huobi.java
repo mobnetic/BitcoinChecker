4db9c74fda47b0a11f4a28159e8e75c3e2a868ce
Bill Chan <biaocy91@gmail.com>
2018-01-06 13:26:52 +0800

    1. Update Huobi API url 2. Update Huobi fetching currency pairs directly from exchange

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Huobi.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Huobi.java
@@ -1,59 +1,60 @@
 package com.mobnetic.coinguardian.model.market;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-
-import org.json.JSONObject;
-
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
-import com.mobnetic.coinguardian.model.currency.Currency;
-import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
-import com.mobnetic.coinguardian.util.ParseUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class Huobi extends Market {
 
 	private final static String NAME = "Huobi";
 	private final static String TTS_NAME = NAME;
-	private final static String URL_BTC = "http://api.huobi.com/staticmarket/ticker_btc_json.js";
-	private final static String URL_BTC_USD = "http://api.huobi.com/usdmarket/ticker_btc_json.js";
-	private final static String URL_LTC = "http://api.huobi.com/staticmarket/ticker_ltc_json.js";
-	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
-	static {
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
-				Currency.CNY,
-				Currency.USD
-			});
-		CURRENCY_PAIRS.put(VirtualCurrency.LTC, new String[]{
-				Currency.CNY
-			});
-	}
+	private final static String URL = "https://api.huobi.pro/market/detail/merged?symbol=%s%s";
+	private final static String URL_CURRENCY_PAIRS = "https://api.huobi.pro/v1/common/symbols";
 	
 	public Huobi() {
-		super(NAME, TTS_NAME, CURRENCY_PAIRS);
+		super(NAME, TTS_NAME, null);
 	}
 
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		if(VirtualCurrency.LTC.equals(checkerInfo.getCurrencyBase())) {
-			return URL_LTC;
-		} else if(Currency.USD.equals(checkerInfo.getCurrencyCounter())) {
-			return URL_BTC_USD;
-		} else {
-			return URL_BTC;
-		}
+		return String.format(URL, checkerInfo.getCurrencyBaseLowerCase(), checkerInfo.getCurrencyCounterLowerCase());
 	}
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		final JSONObject tickerJsonObject = jsonObject.getJSONObject("ticker");
-		
-		ticker.bid = ParseUtils.getDoubleFromString(tickerJsonObject, "buy");
-		ticker.ask = ParseUtils.getDoubleFromString(tickerJsonObject, "sell");
-		ticker.vol = ParseUtils.getDoubleFromString(tickerJsonObject, "vol");
-		ticker.high = ParseUtils.getDoubleFromString(tickerJsonObject, "high");
-		ticker.low = ParseUtils.getDoubleFromString(tickerJsonObject, "low");
-		ticker.last = ParseUtils.getDoubleFromString(tickerJsonObject, "last");
+		final JSONObject tickerJsonObject = jsonObject.getJSONObject("tick");
+
+		ticker.bid = tickerJsonObject.getJSONArray("bid").getDouble(0);
+		ticker.ask = tickerJsonObject.getJSONArray("ask").getDouble(0);
+		ticker.vol = tickerJsonObject.getDouble("vol");
+		ticker.high = tickerJsonObject.getDouble("high");
+		ticker.low = tickerJsonObject.getDouble("low");
+		ticker.last = tickerJsonObject.getDouble("close");
+	}
+
+	@Override
+	public String getCurrencyPairsUrl(int requestId) {
+		return URL_CURRENCY_PAIRS;
+	}
+
+	@Override
+	protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+		if("ok".equalsIgnoreCase(jsonObject.getString("status"))) {
+			final JSONArray data = jsonObject.getJSONArray("data");
+
+			for(int i = 0; i < data.length(); i++) {
+				String base = data.getJSONObject(i).getString("base-currency");
+				String counter = data.getJSONObject(i).getString("quote-currency");
+				pairs.add(new CurrencyPairInfo(base, counter, base + counter));
+			}
+		} else {
+			throw new Exception("Parse currency pairs error.");
+		}
 	}
 }
