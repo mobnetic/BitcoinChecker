cda0091e3521b2f07ffc7b57fb4ce4575f2a40ae
Maciej Ciemięga <MaciejCiemiega@users.noreply.github.com>
2018-01-10 22:48:49 +0100

    Updated CoinJar API and added dynamic currency pairs

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/CoinJar.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/CoinJar.java
@@ -1,39 +1,24 @@
 package com.mobnetic.coinguardian.model.market;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-
-import org.json.JSONObject;
-
 import com.mobnetic.coinguardian.model.CheckerInfo;
+import com.mobnetic.coinguardian.model.CurrencyPairInfo;
 import com.mobnetic.coinguardian.model.Market;
 import com.mobnetic.coinguardian.model.Ticker;
-import com.mobnetic.coinguardian.model.currency.Currency;
-import com.mobnetic.coinguardian.model.currency.VirtualCurrency;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class CoinJar extends Market {
 
 	private final static String NAME = "CoinJar";
 	private final static String TTS_NAME = "Coin Jar";
-	private final static String URL = "https://coinjar-data.herokuapp.com/fair_rate.json";
-	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
-	static {
-		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
-				Currency.USD,
-				Currency.AUD,
-				Currency.NZD,
-				Currency.CAD,
-				Currency.EUR,
-				Currency.GBP,
-				Currency.SGD,
-				Currency.HKD,
-				Currency.CHF,
-				Currency.JPY
-			});
-	}
-	
+	private final static String URL = "https://api.coinjar.com/v3/exchange_rates";
+	private final static String URL_CURRENCY_PAIRS = URL;
+
 	public CoinJar() {
-		super(NAME, TTS_NAME, CURRENCY_PAIRS);
+		super(NAME, TTS_NAME, null);
 	}
 
 	@Override
@@ -43,14 +28,34 @@ public class CoinJar extends Market {
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		final String currencyCounter = checkerInfo.getCurrencyCounter();
-		ticker.bid = getPriceFromJsonObject(jsonObject, "bid", currencyCounter);
-		ticker.ask = getPriceFromJsonObject(jsonObject, "ask", currencyCounter);
-		ticker.last = getPriceFromJsonObject(jsonObject, "spot", currencyCounter);
+		final JSONObject ratesJsonObject = jsonObject.getJSONObject("exchange_rates");
+		final JSONObject pairJsonObject = ratesJsonObject.getJSONObject(checkerInfo.getCurrencyPairId());
+		ticker.bid = pairJsonObject.getDouble("bid");
+		ticker.ask = pairJsonObject.getDouble("ask");
+		ticker.last = pairJsonObject.getDouble("midpoint");
 	}
 	
-	private double getPriceFromJsonObject(JSONObject jsonObject, String innerObjectName, String currencyCounter) throws Exception {
-		JSONObject innerJsonObject = jsonObject.getJSONObject(innerObjectName);
-		return innerJsonObject.getDouble(currencyCounter);
+	// ====================
+	// Get currency pairs
+	// ====================
+	@Override
+	public String getCurrencyPairsUrl(int requestId) {
+		return URL_CURRENCY_PAIRS;
+	}
+
+	@Override
+	protected void parseCurrencyPairsFromJsonObject(int requestId, JSONObject jsonObject, List<CurrencyPairInfo> pairs) throws Exception {
+		final JSONObject ratesJsonObject = jsonObject.getJSONObject("exchange_rates");
+		final JSONArray namesJsonArray = ratesJsonObject.names();
+
+		for(int i=0; i< namesJsonArray.length(); ++i) {
+			final String symbol = namesJsonArray.getString(i);
+			final JSONObject pairJsonObject = ratesJsonObject.getJSONObject(symbol);
+			pairs.add(new CurrencyPairInfo(
+					pairJsonObject.getString("base_currency"),
+					pairJsonObject.getString("counter_currency"),
+					symbol
+			));
+		}
 	}
 }
