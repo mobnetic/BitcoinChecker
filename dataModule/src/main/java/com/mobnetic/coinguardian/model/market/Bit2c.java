e781c45304c90fba582a19aa42267d4b0361f6e6
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2018-08-05 13:01:19 +0300

    feat(bit2c): add the ETH/NIS and ETC/NIS pairs

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bit2c.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/Bit2c.java
@@ -17,14 +17,22 @@ public class Bit2c extends Market {
 	private final static String TTS_NAME = "Bit 2c";
 	private final static String URL = "https://www.bit2c.co.il/Exchanges/%1$s%2$s/Ticker.json";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<>();
+
+	//GET https://bit2c.co.il/Exchanges/[BtcNis/EthNis/BchNis/LtcNis/EtcNis/BtgNis]/Ticker.json
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
+				Currency.NIS
+		});
+		CURRENCY_PAIRS.put(VirtualCurrency.ETH, new String[]{
+				Currency.NIS
+		});
+		CURRENCY_PAIRS.put(VirtualCurrency.BCH, new String[]{
 				Currency.NIS
 		});
 		CURRENCY_PAIRS.put(VirtualCurrency.LTC, new String[]{
 				Currency.NIS
 		});
-		CURRENCY_PAIRS.put(VirtualCurrency.BCH, new String[]{
+		CURRENCY_PAIRS.put(VirtualCurrency.ETC, new String[]{
 				Currency.NIS
 		});
 		CURRENCY_PAIRS.put(VirtualCurrency.BTG, new String[]{
@@ -40,7 +48,7 @@ public class Bit2c extends Market {
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
 		return String.format(URL, checkerInfo.getCurrencyBase(), checkerInfo.getCurrencyCounter());
 	}
-	
+
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
 		ticker.bid = jsonObject.getDouble("h");
