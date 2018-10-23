f4da7f5e48eaeb19af999716e9023750b5b11c4f
Jorge Pereira <jpereira@users.noreply.github.com>
2018-10-23 17:17:34 -0300

    Remove Foxbit due to instability (#796)
    The exchange www.foxbit.com.br is no longer running
    over the Blinktrade network.

--- dataModule/src/main/java/com/mobnetic/coinguardian/config/MarketsConfig.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/config/MarketsConfig.java
@@ -87,7 +87,6 @@ public class MarketsConfig {
 		addMarket(new HitBtc());
 		addMarket(new CleverCoin());
 		addMarket(new BitBay());
-		addMarket(new FoxBit());
 		addMarket(new QuadrigaCX());
 		addMarket(new CoinMateIO());
 		addMarket(new BitMarket24PL());
