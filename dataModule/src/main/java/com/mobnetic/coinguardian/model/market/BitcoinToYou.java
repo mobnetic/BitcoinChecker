0906486c0d0d572856a9face6c42d9ec47399f97
Alexander Biryukov <aabiryukov@gmail.com>
2020-08-29 02:21:52 +0300

    Fixed BitcoinToYou exchange

--- dataModule/src/main/java/com/mobnetic/coinguardian/model/market/BitcoinToYou.java
+++ dataModule/src/main/java/com/mobnetic/coinguardian/model/market/BitcoinToYou.java
@@ -15,7 +15,7 @@ public class BitcoinToYou extends Market {
 
 	private final static String NAME = "BitcoinToYou";
 	private final static String TTS_NAME = "Bitcoin To You";
-	private final static String URL = "https://www.bitcointoyou.com/api/ticker.aspx";
+	private final static String URL = "https://back.bitcointoyou.com/api/ticker?pair=%1$s_%2$sC";
 	private final static HashMap<String, CharSequence[]> CURRENCY_PAIRS = new LinkedHashMap<String, CharSequence[]>();
 	static {
 		CURRENCY_PAIRS.put(VirtualCurrency.BTC, new String[]{
@@ -29,15 +29,22 @@ public class BitcoinToYou extends Market {
 
 	@Override
 	public String getUrl(int requestId, CheckerInfo checkerInfo) {
-		return URL;
+/*
+		String pairId = checkerInfo.getCurrencyPairId();
+		if (pairId == null) {
+			pairId = String.format("%1$s_%2$s", checkerInfo.getCurrencyBaseLowerCase(), checkerInfo.getCurrencyCounterLowerCase());
+		}
+ */
+		String result = String.format(URL, checkerInfo.getCurrencyBase(), checkerInfo.getCurrencyCounter());
+		return result;
 	}
 	
 	@Override
 	protected void parseTickerFromJsonObject(int requestId, JSONObject jsonObject, Ticker ticker, CheckerInfo checkerInfo) throws Exception {
-		final JSONObject tickerJsonObject = jsonObject.getJSONObject("ticker");
-		ticker.bid = tickerJsonObject.getDouble("buy");
-		ticker.ask = tickerJsonObject.getDouble("sell");
-		ticker.vol = tickerJsonObject.getDouble("vol");
+		final JSONObject tickerJsonObject = jsonObject.getJSONObject("summary");
+//		ticker.bid = tickerJsonObject.getDouble("buy");
+//		ticker.ask = tickerJsonObject.getDouble("sell");
+		ticker.vol = tickerJsonObject.getDouble("amount");
 		ticker.high = tickerJsonObject.getDouble("high");
 		ticker.low = tickerJsonObject.getDouble("low");
 		ticker.last = tickerJsonObject.getDouble("last");
