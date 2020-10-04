f9969bc16d5fc14353fc20d9176d9d238bd1a477
Alexander Biryukov <aabiryukov@gmail.com>
2020-10-05 02:23:06 +0300

    Deleted obsolete exchanges

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BitoPro.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BitoPro.kt
@@ -1,23 +1,39 @@
 package com.aneonex.bitcoinchecker.datamodule.model.market
 
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
+import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
 import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
 import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency
 import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
 import com.aneonex.bitcoinchecker.datamodule.model.currency.CurrencyPairsMap
 import org.json.JSONArray
+import org.json.JSONObject
 
 class BitoEX : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
     companion object {
-        private const val NAME = "BitoEX"
+        private const val NAME = "BitoPro"
         private const val TTS_NAME = NAME
         private const val URL = "https://www.bitoex.com/sync/dashboard/%1\$s"
-        private val CURRENCY_PAIRS: CurrencyPairsMap = CurrencyPairsMap()
+        private const val URL_CURRENCY_PAIRS = "https://api.bitopro.com/v3/provisioning/trading-pairs"
+    }
 
-        init {
-            CURRENCY_PAIRS[VirtualCurrency.BTC] = arrayOf(
-                    Currency.TWD
+    override fun getCurrencyPairsUrl(requestId: Int): String? {
+        return URL_CURRENCY_PAIRS
+    }
+
+    override fun parseCurrencyPairsFromJsonObject(requestId: Int, jsonObject: JSONObject, pairs: MutableList<CurrencyPairInfo>) {
+        val dataJson = jsonObject.getJSONArray("data")
+        for(i in 0 until dataJson.length()){
+            val pairJson = dataJson.getJSONObject(i)
+            if(!pairJson.getBoolean("maintain"))
+                continue
+
+            pairs.add(CurrencyPairInfo(
+                    pairJson.getString("base"),
+                    pairJson.getString("quote"),
+                    pairJson.getString("pair")
+                )
             )
         }
     }
