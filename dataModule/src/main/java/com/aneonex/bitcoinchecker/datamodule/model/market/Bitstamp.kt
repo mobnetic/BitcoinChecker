24dfb46a5a1013df9ce69a08b1f2095ef0d07669
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2021-01-06 19:20:30 +0300

    Bitstamp: added assets synchronization

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/Bitstamp.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/Bitstamp.kt
@@ -1,18 +1,22 @@
 package com.aneonex.bitcoinchecker.datamodule.model.market
 
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
+import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
 import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
 import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency
 import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
 import com.aneonex.bitcoinchecker.datamodule.model.currency.CurrencyPairsMap
+import org.json.JSONArray
 import org.json.JSONObject
 
 class Bitstamp : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
     companion object {
         private const val NAME = "Bitstamp"
         private const val TTS_NAME = NAME
-        private const val URL = "https://www.bitstamp.net/api/v2/ticker/%1\$s%2\$s"
+        private const val URL_TICKER = "https://www.bitstamp.net/api/v2/ticker/%1\$s"
+        private const val URL_CURRENCY_PAIRS = "https://www.bitstamp.net/api/v2/trading-pairs-info/"
+
         private val CURRENCY_PAIRS: CurrencyPairsMap = CurrencyPairsMap()
 
         init {
@@ -62,8 +66,33 @@ class Bitstamp : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
         }
     }
 
+    override fun getCurrencyPairsUrl(requestId: Int): String {
+        return URL_CURRENCY_PAIRS
+    }
+
+    override fun parseCurrencyPairs(requestId: Int, responseString: String, pairs: MutableList<CurrencyPairInfo>) {
+        val markets = JSONArray(responseString)
+
+        for(i in 0 until markets.length()){
+            val market = markets.getJSONObject(i)
+
+            if(market.getString("trading") == "Enabled") {
+                val assetsInPair = market.getString("name").split('/')
+                if (assetsInPair.size != 2) continue
+
+                pairs.add(CurrencyPairInfo(
+                        assetsInPair[0], // base
+                        assetsInPair[1], // quote
+                        market.getString("url_symbol")
+                ))
+            }
+        }
+    }
+
     override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyBaseLowerCase, checkerInfo.currencyCounterLowerCase)
+        // Compatibility with old pairs
+        val pairId = checkerInfo.currencyPairId ?: (checkerInfo.currencyBaseLowerCase + checkerInfo.currencyCounterLowerCase)
+        return String.format(URL_TICKER, pairId)
     }
 
     @Throws(Exception::class)
