2e0b4ae2d58cf404db987c44e48d025fa5c0b093
andi-klein <andiklein@gmx.com>
2021-04-16 16:36:03 +1000

    BtcMarkets.net updated to API v3 (#108)
    * Update BtcMarkets.kt
    
    Remove discontinued *-BTC pairs and add numerous new cryptocurrencies (COMP, ALGO, MCAU, USDT, POWR, OMG, BSV, GNT, BAT, XLM, ENJ, LINK)
    
    * BtcMarkets.net updated to API v3
    
    Co-authored-by: Aneonex Software <69394046+aneonex@users.noreply.github.com>

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BtcMarkets.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BtcMarkets.kt
@@ -1,56 +1,52 @@
 package com.aneonex.bitcoinchecker.datamodule.model.market
 
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
-import com.aneonex.bitcoinchecker.datamodule.model.Market
+import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.CurrencyPairsMap
+import com.aneonex.bitcoinchecker.datamodule.model.market.generic.SimpleMarket
+import com.aneonex.bitcoinchecker.datamodule.util.TimeUtils
+import org.json.JSONArray
 import org.json.JSONObject
 
-class BtcMarkets : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
-    companion object {
-        private const val NAME = "BtcMarkets.net"
-        private const val TTS_NAME = "BTC Markets net"
-        private const val URL = "https://api.btcmarkets.net/market/%1\$s/%2\$s/tick"
-        private val CURRENCY_PAIRS: CurrencyPairsMap = CurrencyPairsMap()
-
-        init {
-            CURRENCY_PAIRS[VirtualCurrency.BTC] = arrayOf(
-                    Currency.AUD
-            )
-            CURRENCY_PAIRS[VirtualCurrency.LTC] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.AUD
-            )
-            CURRENCY_PAIRS[VirtualCurrency.ETC] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.AUD
-            )
-            CURRENCY_PAIRS[VirtualCurrency.ETH] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.AUD
-            )
-            CURRENCY_PAIRS[VirtualCurrency.XRP] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.AUD
-            )
-            CURRENCY_PAIRS[VirtualCurrency.BCH] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.AUD
-            )
-        }
+class BtcMarkets : SimpleMarket(
+        "BtcMarkets.net",
+        "https://api.btcmarkets.net/v3/markets",
+        "https://api.btcmarkets.net/v3/markets/%1\$s/ticker",
+        "BTC Markets net"
+) {
+    override fun getPairId(checkerInfo: CheckerInfo): String {
+        // Compatibility with old app version
+        return checkerInfo.currencyPairId ?: "${checkerInfo.currencyBase}-${checkerInfo.currencyCounter}"
     }
 
-    override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyBase, checkerInfo.currencyCounter)
+    override fun parseCurrencyPairs(requestId: Int, responseString: String, pairs: MutableList<CurrencyPairInfo>) {
+        val markets = JSONArray(responseString)
+
+        for (i in 0 until markets.length()) {
+            val market = markets.getJSONObject(i)
+
+            pairs.add(CurrencyPairInfo(
+                    market.getString("baseAssetName"),
+                    market.getString("quoteAssetName"),
+                    market.getString("marketId")
+            ))
+        }
     }
 
     @Throws(Exception::class)
     override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
-        ticker.bid = jsonObject.getDouble("bestBid")
-        ticker.ask = jsonObject.getDouble("bestAsk")
-        ticker.last = jsonObject.getDouble("lastPrice")
-        ticker.timestamp = jsonObject.getLong("timestamp")
+        ticker.apply {
+            last = jsonObject.getDouble("lastPrice")
+            bid = jsonObject.getDouble("bestBid")
+            ask = jsonObject.getDouble("bestAsk")
+
+            low = jsonObject.getDouble("low24h")
+            high = jsonObject.getDouble("high24h")
+
+            vol = jsonObject.getDouble("volume24h")
+            // volQuote = jsonObject.getDouble("volumeQte24h")
+
+            timestamp = TimeUtils.convertISODateToTimestamp(jsonObject.getString("timestamp"))
+        }
     }
 }
