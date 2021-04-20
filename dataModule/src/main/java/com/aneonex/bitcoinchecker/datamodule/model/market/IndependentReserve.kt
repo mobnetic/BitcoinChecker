39a56948e746f3ba1a373ce27287f27547df9a9f
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2021-04-20 19:11:09 +0300

    IndependentReserve: fixed ticker data and improved pairs sync

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/IndependentReserve.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/IndependentReserve.kt
@@ -4,9 +4,6 @@ import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
 import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
 import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency.AUD
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency.NZD
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency.USD
 import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
 import org.json.JSONArray
 import org.json.JSONObject
@@ -17,49 +14,69 @@ class IndependentReserve : Market(NAME, TTS_NAME, null) {
     companion object {
         private const val NAME = "Independent Reserve"
         private const val TTS_NAME = NAME
-        private const val URL = "https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=%1\$s&secondaryCurrencyCode=%2\$s"
-        private const val URL_CURRENCY_PAIRS = "https://api.independentreserve.com/Public/GetValidPrimaryCurrencyCodes"
-        private val SUPPORTED_SECONDARY_CURRENCY_CODES = listOf(AUD, USD, NZD)
+        private const val URL_TICKER = "https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=%1\$s&secondaryCurrencyCode=%2\$s"
+        private const val URL_CURRENCY_PRIMARY_CODES = "https://api.independentreserve.com/Public/GetValidPrimaryCurrencyCodes"
+        private const val URL_CURRENCY_SECONDARY_CODES = "https://api.independentreserve.com/Public/GetValidSecondaryCurrencyCodes"
 
         private fun getCurrencyPublicName(currency: String): String{
             return if(currency == "XBT")  VirtualCurrency.BTC else currency
         }
     }
 
+    private val latestPrimaryCurrencies = mutableListOf<String>()
+
+    override val currencyPairsNumOfRequests: Int = 2
+
+    override fun getCurrencyPairsUrl(requestId: Int): String {
+        return if (requestId == 0) URL_CURRENCY_PRIMARY_CODES else URL_CURRENCY_SECONDARY_CODES
+    }
+
+
     override fun parseCurrencyPairs(requestId: Int, responseString: String, pairs: MutableList<CurrencyPairInfo>) {
-        val primaryCurrencies = JSONArray(responseString)
+        val currencyCodes = JSONArray(responseString)
 
-        for (i in 0 until primaryCurrencies.length()) {
-            val primaryCurrencyCode = primaryCurrencies.getString(i)
+        // Handling primary currencies
+        if (requestId == 0) {
+            latestPrimaryCurrencies.clear()
 
-            SUPPORTED_SECONDARY_CURRENCY_CODES.forEach { secondaryPair ->
-                pairs.add(CurrencyPairInfo(
-                        getCurrencyPublicName(primaryCurrencyCode.toUpperCase(Locale.ROOT)),
-                        secondaryPair.toUpperCase(Locale.ROOT),
-                        primaryCurrencyCode.toLowerCase(Locale.ROOT),
-                ))
+            for (i in 0 until currencyCodes.length()) {
+                latestPrimaryCurrencies.add(currencyCodes.getString(i))
             }
+        }
+        // Handling secondary currencies
+        else {
+            for (i in 0 until currencyCodes.length()) {
+                val secondaryCurrencyCode = currencyCodes.getString(i)
+
+                latestPrimaryCurrencies.forEach { primaryCurrency ->
+                    pairs.add(CurrencyPairInfo(
+                            getCurrencyPublicName(primaryCurrency.toUpperCase(Locale.ROOT)),
+                            secondaryCurrencyCode.toUpperCase(Locale.ROOT),
+                            null
+                    ))
+                }
+            }
+
+            latestPrimaryCurrencies.clear()
         }
     }
 
-    // ====================
-    // Get currency pairs
-    // ====================
-    override fun getCurrencyPairsUrl(requestId: Int): String {
-        return URL_CURRENCY_PAIRS
-    }
-
     override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyBase, checkerInfo.currencyCounter)
+        return String.format(URL_TICKER, checkerInfo.currencyBase, checkerInfo.currencyCounter)
     }
 
     @Throws(Exception::class)
     override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
         ticker.bid = jsonObject.getDouble("CurrentHighestBidPrice")
-        ticker.vol = jsonObject.getDouble("DayVolumeXbt")
-        ticker.high = jsonObject.getDouble("CurrentHighestBidPrice")
-        ticker.low = jsonObject.getDouble("CurrentLowestOfferPrice")
+        ticker.ask = jsonObject.getDouble("CurrentLowestOfferPrice")
+
+        ticker.high = jsonObject.getDouble("DayHighestPrice")
+        ticker.low = jsonObject.getDouble("DayLowestPrice")
+
         ticker.last = jsonObject.getDouble("LastPrice")
+        @Suppress("SpellCheckingInspection")
+        ticker.vol = jsonObject.getDouble("DayVolumeXbtInSecondaryCurrrency")
+
         ticker.timestamp = Instant.parse(jsonObject.getString("CreatedTimestampUtc")).toEpochMilli()
     }
 }
