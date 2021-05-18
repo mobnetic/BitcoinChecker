821480b5bc9e0c4f43aec705a17ded6ab03b0854
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2021-05-19 01:26:16 +0300

    Infra: Update Kotlin and Gradle to the latest version

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/CheckerInfo.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/CheckerInfo.kt
@@ -5,7 +5,7 @@ import java.util.*
 class CheckerInfo(currencyBase: String, currencyCounter: String, currencyPairId: String?, val contractType: Int)
     : CurrencyPairInfo(currencyBase, currencyCounter, currencyPairId) {
     val currencyBaseLowerCase: String
-        get() = currencyBase.toLowerCase(Locale.US)
+        get() = currencyBase.lowercase(Locale.US)
     val currencyCounterLowerCase: String
-        get() = currencyCounter.toLowerCase(Locale.US)
+        get() = currencyCounter.lowercase(Locale.US)
 }
