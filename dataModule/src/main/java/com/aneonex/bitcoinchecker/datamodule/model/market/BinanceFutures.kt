3ecae1c1d56171d20e4011401619a5a0af7d354c
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2021-06-25 00:53:04 +0300

    Added Binance COIN-M perpetual futures

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BinanceFutures.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BinanceFutures.kt
@@ -5,32 +5,65 @@ import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
 import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
 import com.aneonex.bitcoinchecker.datamodule.util.forEachJSONObject
+import org.json.JSONArray
 import org.json.JSONObject
 
 class BinanceFutures : Market(NAME, TTS_NAME, null) {
     companion object {
         private const val NAME = "Binance Futures"
         private const val TTS_NAME = NAME
-        private const val URL = "https://fapi.binance.com/fapi/v1/ticker/24hr?symbol=%1\$s"
-        private const val URL_CURRENCY_PAIRS = "https://fapi.binance.com/fapi/v1/exchangeInfo"
+
+        private const val URL_USD_M = "https://fapi.binance.com/fapi/v1/ticker/24hr?symbol=%1\$s"
+        private const val URL_CURRENCY_PAIRS_USD_M = "https://fapi.binance.com/fapi/v1/exchangeInfo"
+
+        private const val URL_COIN_M = "https://dapi.binance.com/dapi/v1/ticker/24hr?symbol=%1\$s"
+        private const val URL_CURRENCY_PAIRS_COIN_M = "https://dapi.binance.com/dapi/v1/exchangeInfo"
+
+        private const val COIN_M_PREFIX = "2:"
+
+        private fun isCoinMPair(pairId: String) = pairId.startsWith(COIN_M_PREFIX)
+
+        private fun parseTicker(jsonObject: JSONObject, ticker: Ticker) {
+            jsonObject.apply {
+                ticker.vol = getDouble("volume")
+                ticker.high = getDouble("highPrice")
+                ticker.low = getDouble("lowPrice")
+                ticker.last = getDouble("lastPrice")
+                ticker.timestamp = getLong("closeTime")
+
+                // Optional
+                ticker.volQuote = optDouble("quoteVolume", ticker.volQuote)
+            }
+        }
     }
 
     override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyPairId)
+        return if(isCoinMPair(checkerInfo.currencyPairId!!)) {
+            val pairId = checkerInfo.currencyPairId.substring(COIN_M_PREFIX.length)
+            String.format(URL_COIN_M, pairId)
+        }
+        else {
+            String.format(URL_USD_M, checkerInfo.currencyPairId)
+        }
     }
 
-    @Throws(Exception::class)
-    override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
-        ticker.vol = jsonObject.getDouble("volume")
-        ticker.high = jsonObject.getDouble("highPrice")
-        ticker.low = jsonObject.getDouble("lowPrice")
-        ticker.last = jsonObject.getDouble("lastPrice")
-        ticker.timestamp = jsonObject.getLong("closeTime")
+    override fun parseTicker(
+        requestId: Int,
+        responseString: String,
+        ticker: Ticker,
+        checkerInfo: CheckerInfo
+    ) {
+        if(isCoinMPair(checkerInfo.currencyPairId!!))
+            parseTicker(JSONArray(responseString).getJSONObject(0), ticker)
+        else
+            parseTicker(JSONObject(responseString), ticker)
     }
 
-    override fun getCurrencyPairsUrl(requestId: Int): String {
-        return URL_CURRENCY_PAIRS
-    }
+    override val currencyPairsNumOfRequests: Int
+        get() = 2
+
+    override fun getCurrencyPairsUrl(requestId: Int): String =
+        if(requestId == 0) URL_CURRENCY_PAIRS_USD_M else URL_CURRENCY_PAIRS_COIN_M
 
     @Throws(Exception::class)
     override fun parseCurrencyPairsFromJsonObject(requestId: Int, jsonObject: JSONObject, pairs: MutableList<CurrencyPairInfo>) {
@@ -39,7 +72,9 @@ class BinanceFutures : Market(NAME, TTS_NAME, null) {
             if(marketJsonObject.getString("contractType") != "PERPETUAL")
                 return@forEachJSONObject
 
-            val symbol = marketJsonObject.getString("symbol")
+            val symbol = marketJsonObject.getString("symbol").let {
+                if(requestId > 0) COIN_M_PREFIX + it else it
+            }
             val baseAsset = marketJsonObject.getString("baseAsset")
             val quoteAsset = marketJsonObject.getString("quoteAsset")
 
