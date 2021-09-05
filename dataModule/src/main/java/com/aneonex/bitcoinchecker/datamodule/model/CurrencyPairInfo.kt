84cd19206b756926f9fe21ba37c67c1a79f289c5
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2021-09-05 14:03:30 +0300

    Added constructor for safe gson deserialization

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/CurrencyPairInfo.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/CurrencyPairInfo.kt
@@ -1,9 +1,20 @@
 package com.aneonex.bitcoinchecker.datamodule.model
 
-open class CurrencyPairInfo(val currencyBase: String, val currencyCounter: String, val currencyPairId: String?) : Comparable<CurrencyPairInfo> {
+open class CurrencyPairInfo(
+    val currencyBase: String,
+    val currencyCounter: String,
+    val currencyPairId: String?
+) : Comparable<CurrencyPairInfo> {
+
+    @Suppress("unused") // Used by Gson
+    private constructor() : this("", "", null)
+
     @Throws(NullPointerException::class)
     override fun compareTo(other: CurrencyPairInfo): Int {
         val compBase = currencyBase.compareTo(other.currencyBase, ignoreCase = true)
-        return if (compBase != 0) compBase else currencyCounter.compareTo(other.currencyCounter, ignoreCase = true)
+        return if (compBase != 0) compBase else currencyCounter.compareTo(
+            other.currencyCounter,
+            ignoreCase = true
+        )
     }
 }
