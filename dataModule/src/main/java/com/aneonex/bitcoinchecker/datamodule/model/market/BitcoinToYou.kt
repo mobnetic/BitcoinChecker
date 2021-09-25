e0b54083c5107f518c44b3d270480434145988b3
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2021-09-25 10:59:19 +0300

    BitcoinToYou exchange api v2

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BitcoinToYou.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/BitcoinToYou.kt
@@ -1,45 +1,69 @@
 package com.aneonex.bitcoinchecker.datamodule.model.market
 
+import com.aneonex.bitcoinchecker.datamodule.exceptions.MarketParseException
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
-import com.aneonex.bitcoinchecker.datamodule.model.Market
+import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.CurrencyPairsMap
+import com.aneonex.bitcoinchecker.datamodule.model.market.generic.SimpleMarket
+import com.aneonex.bitcoinchecker.datamodule.util.forEachJSONObject
+import org.json.JSONArray
 import org.json.JSONObject
 
-class BitcoinToYou : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
-    companion object {
-        private const val NAME = "BitcoinToYou"
-        private const val TTS_NAME = "Bitcoin To You"
-        private const val URL = "https://back.bitcointoyou.com/api/ticker?pair=%1\$s_%2\$sC"
-        private val CURRENCY_PAIRS: CurrencyPairsMap = CurrencyPairsMap()
+class BitcoinToYou : SimpleMarket(
+    "BitcoinToYou",
+    "https://back.bitcointoyou.com/api/v2/pair/getPairList",
+    "https://back.bitcointoyou.com/api/v2/ticker?pair=%1\$s",
+    "Bitcoin To You"
+) {
 
-        init {
-            CURRENCY_PAIRS[VirtualCurrency.BTC] = arrayOf(
-                    Currency.BRL
-            )
-        }
+    override fun getPairId(checkerInfo: CheckerInfo): String? {
+        return if (checkerInfo.currencyPairId == null)
+                    with(checkerInfo) { "${currencyBase}_${currencyCounter}C" }
+                else
+                    super.getPairId(checkerInfo)
     }
 
-    override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-/*
-		String pairId = checkerInfo.getCurrencyPairId();
-		if (pairId == null) {
-			pairId = String.format("%1$s_%2$s", checkerInfo.getCurrencyBaseLowerCase(), checkerInfo.getCurrencyCounterLowerCase());
-		}
- */
-        return String.format(URL, checkerInfo.currencyBase, checkerInfo.currencyCounter)
+    override fun parseCurrencyPairs(
+        requestId: Int,
+        responseString: String,
+        pairs: MutableList<CurrencyPairInfo>
+    ) {
+        fun getDisplaySymbol(symbolString: String): String {
+            @Suppress("SpellCheckingInspection")
+            return when(symbolString){
+                "CBRL" -> "BRL"
+                "BRLC" -> "BRL"
+                else -> symbolString
+            }
+        }
+
+        val data = JSONArray(responseString)
+        data.forEachJSONObject {
+            val symbol = it.getString("symbol")
+            val assets = symbol.split('_')
+            if(assets.size == 2) {
+                pairs.add(
+                    CurrencyPairInfo(
+                        getDisplaySymbol(assets[0]),
+                        getDisplaySymbol(assets[1]),
+                        symbol
+                    )
+                )
+            }
+        }
     }
 
     @Throws(Exception::class)
     override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
-        val tickerJsonObject = jsonObject.getJSONObject("summary")
-        //		ticker.bid = tickerJsonObject.getDouble("buy");
-//		ticker.ask = tickerJsonObject.getDouble("sell");
-        ticker.vol = tickerJsonObject.getDouble("amount")
-        ticker.high = tickerJsonObject.getDouble("high")
-        ticker.low = tickerJsonObject.getDouble("low")
-        ticker.last = tickerJsonObject.getDouble("last")
+        jsonObject.getJSONObject("summary").also { data ->
+            ticker.vol = data.getDouble("amount")
+            if(ticker.vol <= 0)
+                throw MarketParseException("No trading volume")
+
+            ticker.volQuote = data.getDouble("quote_volume")
+            ticker.high = data.getDouble("high")
+            ticker.low = data.getDouble("low")
+            ticker.last = data.getDouble("last")
+        }
     }
 }
